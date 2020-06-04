8c6f889ff3ac0c8be2cbb3be705ae87fdd808b31
devx3 <erickfabiani123@gmail.com>
2020-06-04 17:47:03 +0000

    COMMIT INICIAL

---
+++ docker.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# Docker installed! 
+clear
+echo "Docker Installed!"
+docker --version
+sleep 1
+
+echo "Installing docker-compose..."
+sleep 2
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+echo "Docker Compose Installed"
+docker-compose --version
+sleep 2
+
